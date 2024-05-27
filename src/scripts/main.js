020f314abfa97c26bf96002c48e0de0c3686e1a0
ANA FLÁVIA BUENO GOMES <06006399@acad.unifeso.lan>
2024-05-27 11:03:17 -0300

    feat: new style for final icons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,35 @@
 document.querySelectorAll('.main-characters img').forEach(img => {
 
     img.addEventListener('click', function() {
-
         this.style.animation = 'bounce 0.6s cubic-bezier(0.28, 0.84, 0.42, 1)';
+
         this.addEventListener('animationend', function() {
             this.style.animation = '';
-        }, 
-
-        { 
-            once: true 
-        });
-        
+        }, { once: true });
     });
 
+});
+
+document.getElementById('first-animation').addEventListener('click', function() {
+    this.style.animation = 'leftrotate 1s infinite linear';
+    
+    this.addEventListener('animationend', function() {
+        this.style.animation = '';
+    }, { once: true });
+});
+
+document.getElementById('separate-animation').addEventListener('click', function() {
+    this.style.animation = 'pulse 0.5s infinite alternate';
+    
+    this.addEventListener('animationend', function() {
+        this.style.animation = '';
+    }, { once: true });
+});
+
+document.getElementById('second-animation').addEventListener('click', function() {
+    this.style.animation = 'rightrotate 1s infinite linear';
+    
+    this.addEventListener('animationend', function() {
+        this.style.animation = '';
+    }, { once: true });
 });

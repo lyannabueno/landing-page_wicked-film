8365b6070bd0a282f83a2d7cc2ce0947851b0d58
ANA FLÁVIA BUENO GOMES <06006399@acad.unifeso.lan>
2024-05-29 18:48:42 -0300

    creating countdown

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -33,3 +33,34 @@ document.getElementById('second-animation').addEventListener('click', function()
         this.style.animation = '';
     }, { once: true });
 });
+
+const dataDoEvento = new Date("Nov 24, 2024 00:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual; // tempo em milessegundos 
+
+    const diasEmMs  = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / (diasEmMs));
+    const horasAteEvento = Math.floor((distanciaAteEvento % (diasEmMs)) / (horaEmMs));
+    const minutosAteEvento = Math.floor((distanciaAteEvento % (horaEmMs)) / (minutosEmMs));
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutosEmMs) / 1000);
+
+    console.log(diasAteEvento);
+    console.log(horasAteEvento);
+    console.log(minutosAteEvento);
+    console.log(segundosAteEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`
+
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('sobre-filme').innerHTML = 'Evento expirado!';
+    }
+}, 1000);
